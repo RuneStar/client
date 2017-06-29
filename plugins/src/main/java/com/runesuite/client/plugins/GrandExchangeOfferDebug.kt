ea5c02b99c3a411368c75bdd2ba8802468a22051
hunterwb <hunterwb@washington.edu>
2017-06-29 15:39:43 -0700

    ge debug plugin

---
+++ plugins/src/main/java/com/runesuite/client/plugins/GrandExchangeOfferDebug.kt
@@ -0,0 +1,26 @@
+package com.runesuite.client.plugins
+
+import com.runesuite.client.dev.plugins.DisposablePlugin
+import com.runesuite.client.dev.plugins.FileReadWriter
+import com.runesuite.client.dev.plugins.Plugin
+import com.runesuite.client.game.live.Canvas
+import com.runesuite.client.game.live.GrandExchangeOffers
+import java.awt.Color
+
+class GrandExchangeOfferDebug : DisposablePlugin<Plugin.Settings>(), FileReadWriter.Yaml<Plugin.Settings> {
+
+    override val defaultSettings = Plugin.Settings()
+
+    override fun create() {
+        super.create()
+        Canvas.Live.repaints.subscribe { g ->
+            val x = 5
+            var y = 40
+            g.color = Color.WHITE
+            GrandExchangeOffers.get().forEach { o ->
+                g.drawString(o.toString(), x, y)
+                y += 13
+            }
+        }
+    }
+}
