7d5de29358a72b64bb6191fbb34e243854bae536
hunterwb <hunterwb@washington.edu>
2017-07-04 05:05:46 -0700

    ClanChat owner added

--- game/src/main/java/com/runesuite/client/game/live/ClanChat.kt
+++ game/src/main/java/com/runesuite/client/game/live/ClanChat.kt
@@ -7,6 +7,8 @@ object ClanChat {
 
     val SIZE = 100
 
+    val owner: String? get() = accessor.clanChatOwner
+
     fun get(): List<ClanMate?> = accessor.clanChat?.map { it?.let { ClanMate(it) } } ?: emptyList()
 
     val all: List<ClanMate> get() = accessor.clanChat?.mapNotNull { it?.let { ClanMate(it) } } ?: emptyList()
