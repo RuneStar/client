1654dfd4316e840aab9e3109365794f52eeafd87
hunterwb <hunterwb@washington.edu>
2017-09-09 19:07:29 -0700

    re-enable gamestate logging plugin

--- plugins/src/main/java/com/runesuite/client/plugins/debug/GameStateLogger.kt
+++ plugins/src/main/java/com/runesuite/client/plugins/debug/GameStateLogger.kt
@@ -1,17 +1,17 @@
-//package com.runesuite.client.plugins.debug
-//
-//import com.runesuite.client.dev.plugins.DisposablePlugin
-//import com.runesuite.client.dev.plugins.Plugin
-//import com.runesuite.client.game.live.Game
-//
-//class GameStateLogger : DisposablePlugin<Plugin.Settings>() {
-//
-//    override val defaultSettings = Plugin.Settings()
-//
-//    override fun start() {
-//        super.start()
-//        add(Game.stateChanges.subscribe {
-//            logger.debug { it }
-//        })
-//    }
-//}
+package com.runesuite.client.plugins.debug
+
+import com.runesuite.client.core.api.live.Game
+import com.runesuite.client.plugins.framework.DisposablePlugin
+import com.runesuite.client.plugins.framework.Plugin
+
+class GameStateLogger : DisposablePlugin<Plugin.Settings>() {
+
+    override val defaultSettings = Plugin.Settings()
+
+    override fun start() {
+        super.start()
+        add(Game.stateChanges.subscribe {
+            logger.debug { it }
+        })
+    }
+}
