a467b507ee4b187eb8113b92bdf15dfc6c1dedb2
hunterwb <hunter@hunterwb.com>
2017-11-05 17:40:41 -0800

    Fix launcher

--- launcher/src/main/java/com/runesuite/client/launcher/LauncherMain.kt
+++ launcher/src/main/java/com/runesuite/client/launcher/LauncherMain.kt
@@ -6,16 +6,28 @@ import com.runesuite.client.common.*
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils
 import org.eclipse.aether.RepositorySystem
 import org.eclipse.aether.artifact.DefaultArtifact
+import org.eclipse.aether.transport.file.FileTransporterFactory
+import org.eclipse.aether.transport.http.HttpTransporterFactory
+import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory
 import org.eclipse.aether.repository.LocalRepository
 import org.eclipse.aether.repository.RemoteRepository
+import org.eclipse.aether.repository.RepositoryPolicy
 import org.eclipse.aether.resolution.ArtifactRequest
 import org.eclipse.aether.resolution.VersionRangeRequest
+import org.eclipse.aether.spi.connector.RepositoryConnectorFactory
+import org.eclipse.aether.spi.connector.transport.TransporterFactory
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 
-private val repoSystem = MavenRepositorySystemUtils.newServiceLocator().getService(RepositorySystem::class.java)
+private val locator =  MavenRepositorySystemUtils.newServiceLocator().apply {
+    addService(TransporterFactory::class.java, FileTransporterFactory::class.java)
+    addService(TransporterFactory::class.java, HttpTransporterFactory::class.java)
+    addService(RepositoryConnectorFactory::class.java, BasicRepositoryConnectorFactory::class.java)
+}
+
+private val repoSystem = locator.getService(RepositorySystem::class.java)
 
 private val localRepo = LocalRepository(Paths.get(System.getProperty("user.home"), ".m2", "repository").toFile())
 
@@ -23,11 +35,10 @@ private val session = MavenRepositorySystemUtils.newSession().apply {
     localRepositoryManager = repoSystem.newLocalRepositoryManager(this, localRepo)
 }
 
-private val remoteRepo = RemoteRepository.Builder(
-        "repo.runesuite.com",
-        "default",
-        "http://repo.runesuite.com/libs-snapshot-local"
-).build()
+private val remoteRepo = RemoteRepository.Builder("repo.runesuite.com", "default", "http://repo.runesuite.com/libs-snapshot-local")
+        .setReleasePolicy(RepositoryPolicy(false, RepositoryPolicy.UPDATE_POLICY_NEVER, RepositoryPolicy.CHECKSUM_POLICY_FAIL))
+        .setSnapshotPolicy(RepositoryPolicy(true, RepositoryPolicy.UPDATE_POLICY_NEVER, RepositoryPolicy.CHECKSUM_POLICY_FAIL))
+        .build()
 
 fun main(args: Array<String>) {
     Files.createDirectories(PLUGINS_SETTINGS_DIR_PATH)
@@ -44,11 +55,18 @@ private fun updateArtifact(artifactId: String, path: Path) {
 }
 
 private fun updateArtifactLocalRepo(artifactId: String): Path {
-    val artifact = DefaultArtifact(GROUP_ID, artifactId, "", "jar", "(,]")
+    val artifact = DefaultArtifact(GROUP_ID, artifactId, null, "jar", "(,]")
     val versionRangeRequest = VersionRangeRequest(artifact, listOf(remoteRepo), null)
-    val version = repoSystem.resolveVersionRange(session, versionRangeRequest).highestVersion.toString()
-    val artifact2 = DefaultArtifact(GROUP_ID, artifactId, "", "jar", version)
-    val artifactRequest = ArtifactRequest(artifact2, listOf(remoteRepo), null)
+    val versionRangeResponse = repoSystem.resolveVersionRange(session, versionRangeRequest)
+    println(versionRangeResponse)
+    val version = versionRangeResponse.highestVersion
+    val artifact2 = DefaultArtifact(GROUP_ID, artifactId, null, "jar", version.toString())
+    val artifactRequestRemoteRepos = if (versionRangeResponse.getRepository(version).id == localRepo.id) {
+        emptyList<RemoteRepository>()
+    } else {
+        listOf(remoteRepo)
+    }
+    val artifactRequest = ArtifactRequest(artifact2, artifactRequestRemoteRepos, null)
     val artifactResponse = repoSystem.resolveArtifact(session, artifactRequest)
     println(artifactResponse)
     return artifactResponse.artifact.file.toPath()
