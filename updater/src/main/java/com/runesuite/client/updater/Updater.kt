8a74a03473f6a86f9c3acaf35da395a350dc6389
hunterwb <hunter@hunterwb.com>
2017-11-10 23:47:34 -0800

    Get current class better in Updater

--- updater/src/main/java/com/runesuite/client/updater/Updater.kt
+++ updater/src/main/java/com/runesuite/client/updater/Updater.kt
@@ -5,10 +5,11 @@ package com.runesuite.client.updater
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.runesuite.client.updater.common.ClassHook
+import java.lang.invoke.MethodHandles
 import java.net.URL
 
 // ClassLoader that loaded this class
-private val classLoader = object {}.javaClass.classLoader
+private val classLoader = MethodHandles.lookup().lookupClass().classLoader
 
 @JvmField
 val HOOKS: List<ClassHook> = jacksonObjectMapper().readValue(classLoader.getResource("hooks.json"))
