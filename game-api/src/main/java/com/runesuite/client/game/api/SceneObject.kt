5a50fbd1f6d288a00fa24244c093ab7b890acded
hunterwb <hunter@hunterwb.com>
2017-12-19 15:46:54 -0800

    Fix SceneObject plane

--- game-api/src/main/java/com/runesuite/client/game/api/SceneObject.kt
+++ game-api/src/main/java/com/runesuite/client/game/api/SceneObject.kt
@@ -1,5 +1,7 @@
 package com.runesuite.client.game.api
 
+import com.runesuite.client.game.api.live.Game
+import com.runesuite.client.game.raw.Client
 import com.runesuite.client.game.raw.Wrapper
 import com.runesuite.client.game.raw.access.XBoundaryObject
 import com.runesuite.client.game.raw.access.XFloorDecoration
@@ -12,7 +14,9 @@ abstract class SceneObject : Wrapper() {
 
     val location get() = tag.location
 
-    class Interactable(override val accessor: XGameObject) : SceneObject() {
+    class Interactable(
+            override val accessor: XGameObject
+    ) : SceneObject() {
 
         override val tag get() = EntityTag(accessor.tag, accessor.plane)
 
@@ -21,27 +25,36 @@ abstract class SceneObject : Wrapper() {
         }
     }
 
-    class Floor(override val accessor: XFloorDecoration) : SceneObject() {
+    class Floor(
+            override val accessor: XFloorDecoration,
+            val plane: Int = Client.accessor.plane
+    ) : SceneObject() {
 
-        override val tag get() = EntityTag(accessor.tag)
+        override val tag get() = EntityTag(accessor.tag, plane)
 
         override fun toString(): String {
             return "SceneObject.Floor(tag=$tag)"
         }
     }
 
-    class Wall(override val accessor: XWallDecoration) : SceneObject() {
+    class Wall(
+            override val accessor: XWallDecoration,
+            val plane: Int = Client.accessor.plane
+    ) : SceneObject() {
 
-        override val tag get() = EntityTag(accessor.tag)
+        override val tag get() = EntityTag(accessor.tag, plane)
 
         override fun toString(): String {
             return "SceneObject.Wall(tag=$tag)"
         }
     }
 
-    class Boundary(override val accessor: XBoundaryObject) : SceneObject() {
+    class Boundary(
+            override val accessor: XBoundaryObject,
+            val plane: Int = Client.accessor.plane
+    ) : SceneObject() {
 
-        override val tag get() = EntityTag(accessor.tag)
+        override val tag get() = EntityTag(accessor.tag, plane)
 
         override fun toString(): String {
             return "SceneObject.Boundary(tag=$tag)"
--- game-api/src/main/java/com/runesuite/client/game/api/live/SceneObjects.kt
+++ game-api/src/main/java/com/runesuite/client/game/api/live/SceneObjects.kt
@@ -8,9 +8,9 @@ object SceneObjects : TileEntities.Many<SceneObject>() {
 
     override fun fromTile(sceneTile: SceneTile, xTile: XTile?): List<SceneObject> {
         val list = ArrayList<SceneObject>()
-        xTile?.floorDecoration?.let { list.add(SceneObject.Floor(it)) }
-        xTile?.wallDecoration?.let { list.add(SceneObject.Wall(it)) }
-        xTile?.boundaryObject?.let { list.add(SceneObject.Boundary(it)) }
+        xTile?.floorDecoration?.let { list.add(SceneObject.Floor(it, sceneTile.plane)) }
+        xTile?.wallDecoration?.let { list.add(SceneObject.Wall(it, sceneTile.plane)) }
+        xTile?.boundaryObject?.let { list.add(SceneObject.Boundary(it, sceneTile.plane)) }
         xTile?.gameObjects?.mapNotNullTo(list) { it?.let { SceneObject.Interactable(it) } }
         return list
     }
@@ -19,7 +19,7 @@ object SceneObjects : TileEntities.Many<SceneObject>() {
 
         override fun fromTile(sceneTile: SceneTile, xTile: XTile?): SceneObject.Wall? {
             val obj = xTile?.wallDecoration ?: return null
-            return SceneObject.Wall(obj)
+            return SceneObject.Wall(obj, sceneTile.plane)
         }
     }
 
@@ -27,7 +27,7 @@ object SceneObjects : TileEntities.Many<SceneObject>() {
 
         override fun fromTile(sceneTile: SceneTile, xTile: XTile?): SceneObject.Floor? {
             val obj = xTile?.floorDecoration ?: return null
-            return SceneObject.Floor(obj)
+            return SceneObject.Floor(obj, sceneTile.plane)
         }
     }
 
@@ -35,7 +35,7 @@ object SceneObjects : TileEntities.Many<SceneObject>() {
 
         override fun fromTile(sceneTile: SceneTile, xTile: XTile?): SceneObject.Boundary? {
             val obj = xTile?.boundaryObject ?: return null
-            return SceneObject.Boundary(obj)
+            return SceneObject.Boundary(obj, sceneTile.plane)
         }
     }
 
