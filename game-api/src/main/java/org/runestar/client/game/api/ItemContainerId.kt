9fd53ad7f20c67b9ed1bc9e552237088ca54fd43
hunterwb <hunter@hunterwb.com>
2017-12-23 16:35:25 -0800

    Add ItemContainer debug plugin

--- game-api/src/main/java/org/runestar/client/game/api/ItemContainerId.kt
+++ game-api/src/main/java/org/runestar/client/game/api/ItemContainerId.kt
@@ -2,6 +2,8 @@ package org.runestar.client.game.api
 
 object ItemContainerId {
 
+    const val PRICE_CHECKER = 90
     const val INVENTORY = 93
+    const val EQUIPMENT = 94
     const val BANK = 95
 }
---
+++ plugins-standard/src/main/java/org/runestar/client/plugins/std/debug/ItemContainersDebug.kt
@@ -0,0 +1,42 @@
+package org.runestar.client.plugins.std.debug
+
+import org.runestar.client.game.api.live.ItemContainers
+import org.runestar.client.game.api.live.LiveCanvas
+import org.runestar.client.plugins.PluginSettings
+import org.runestar.client.plugins.utils.ColorForm
+import org.runestar.client.plugins.utils.DisposablePlugin
+import org.runestar.client.plugins.utils.FontForm
+import java.awt.Font
+
+class ItemContainersDebug : DisposablePlugin<ItemContainersDebug.Settings>() {
+
+    override val defaultSettings = Settings()
+
+    override fun start() {
+        super.start()
+        add(LiveCanvas.repaints.subscribe { g ->
+            val x = 5
+            var y = 40
+            g.font = settings.font.get()
+            g.color = settings.color.get()
+
+
+            val map = ItemContainers.all
+            val strings = ArrayList<String>()
+            map.forEach { k, v ->
+                val vs = v.all.map { "(${it.id}x${it.quantity})" }
+                strings.add("$k:$vs")
+            }
+
+            strings.forEach { s ->
+                g.drawString(s, x, y)
+                y += g.font.size + 5
+            }
+        })
+    }
+
+    class Settings : PluginSettings() {
+        val font = FontForm(Font.SANS_SERIF, FontForm.BOLD, 15f)
+        val color = ColorForm()
+    }
+}
