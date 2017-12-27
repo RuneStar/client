50f10e79f61aecedbab125db4aa9a31b47ab77e3
hunterwb <hunter@hunterwb.com>
2017-12-26 23:15:31 -0800

    Add placeholder HealthBarDefinition fields

--- updater-mapper-standard/src/main/java/org/runestar/client/updater/mapper/std/classes/HealthBarDefinition.kt
+++ updater-mapper-standard/src/main/java/org/runestar/client/updater/mapper/std/classes/HealthBarDefinition.kt
@@ -7,8 +7,11 @@ import org.runestar.client.updater.mapper.extensions.and
 import org.runestar.client.updater.mapper.extensions.predicateOf
 import org.runestar.client.updater.mapper.tree.Class2
 import org.runestar.client.updater.mapper.tree.Method2
-import org.objectweb.asm.Opcodes
-import org.objectweb.asm.Type
+import org.objectweb.asm.Opcodes.*
+import org.objectweb.asm.Type.*
+import org.runestar.client.updater.mapper.OrderMapper
+import org.runestar.client.updater.mapper.extensions.Predicate
+import org.runestar.client.updater.mapper.tree.Instruction2
 
 @DependsOn(HealthBar.definition::class)
 class HealthBarDefinition : IdentityMapper.Class() {
@@ -16,15 +19,51 @@ class HealthBarDefinition : IdentityMapper.Class() {
 
     @DependsOn(Buffer::class)
     class read : IdentityMapper.InstanceMethod() {
-        override val predicate = predicateOf<Method2> { it.returnType == Type.VOID_TYPE }
+        override val predicate = predicateOf<Method2> { it.returnType == VOID_TYPE }
                 .and { it.arguments.startsWith(type<Buffer>()) }
-                .and { it.instructions.none { it.opcode == Opcodes.BIPUSH && it.intOperand == 6 } }
+                .and { it.instructions.none { it.opcode == BIPUSH && it.intOperand == 6 } }
     }
 
     @DependsOn(Buffer::class)
     class readNext : IdentityMapper.InstanceMethod() {
-        override val predicate = predicateOf<Method2> { it.returnType == Type.VOID_TYPE }
+        override val predicate = predicateOf<Method2> { it.returnType == VOID_TYPE }
                 .and { it.arguments.startsWith(type<Buffer>()) }
-                .and { it.instructions.any { it.opcode == Opcodes.BIPUSH && it.intOperand == 6 } }
+                .and { it.instructions.any { it.opcode == BIPUSH && it.intOperand == 6 } }
+    }
+
+    class int1 : OrderMapper.InConstructor.Field(HealthBarDefinition::class, 0) {
+        override val predicate = predicateOf<Instruction2> { it.opcode == PUTFIELD && it.fieldType == INT_TYPE }
+    }
+
+    class int2 : OrderMapper.InConstructor.Field(HealthBarDefinition::class, 1) {
+        override val predicate = predicateOf<Instruction2> { it.opcode == PUTFIELD && it.fieldType == INT_TYPE }
+    }
+
+    class int3 : OrderMapper.InConstructor.Field(HealthBarDefinition::class, 2) {
+        override val predicate = predicateOf<Instruction2> { it.opcode == PUTFIELD && it.fieldType == INT_TYPE }
+    }
+
+    class int4 : OrderMapper.InConstructor.Field(HealthBarDefinition::class, 3) {
+        override val predicate = predicateOf<Instruction2> { it.opcode == PUTFIELD && it.fieldType == INT_TYPE }
+    }
+
+    class int5 : OrderMapper.InConstructor.Field(HealthBarDefinition::class, 4) {
+        override val predicate = predicateOf<Instruction2> { it.opcode == PUTFIELD && it.fieldType == INT_TYPE }
+    }
+
+    class int6 : OrderMapper.InConstructor.Field(HealthBarDefinition::class, 5) {
+        override val predicate = predicateOf<Instruction2> { it.opcode == PUTFIELD && it.fieldType == INT_TYPE }
+    }
+
+    class int7 : OrderMapper.InConstructor.Field(HealthBarDefinition::class, 6) {
+        override val predicate = predicateOf<Instruction2> { it.opcode == PUTFIELD && it.fieldType == INT_TYPE }
+    }
+
+    class int8 : OrderMapper.InConstructor.Field(HealthBarDefinition::class, 7) {
+        override val predicate = predicateOf<Instruction2> { it.opcode == PUTFIELD && it.fieldType == INT_TYPE }
+    }
+
+    class int9 : OrderMapper.InConstructor.Field(HealthBarDefinition::class, 8) {
+        override val predicate = predicateOf<Instruction2> { it.opcode == PUTFIELD && it.fieldType == INT_TYPE }
     }
 }
