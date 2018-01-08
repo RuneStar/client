08e2ba82a2e67a64d68a6334e508cd19a98fd789
hunterwb <hunter@hunterwb.com>
2018-01-07 16:33:23 -0800

    Add Java 8 compatable awt Taskbar functions

---
+++ common/src/main/java/org/runestar/client/common/Taskbar.kt
@@ -0,0 +1,27 @@
+package org.runestar.client.common
+
+import java.awt.Window
+
+private val taskBarClass = catchToNull { Class.forName("java.awt.Taskbar") }
+
+private val getTaskbar = taskBarClass?.let {
+    catchToNull { taskBarClass.getMethod("getTaskbar") }
+}
+
+private val taskbar = catchToNull { getTaskbar?.invoke(null) }
+
+private val setWindowProgressValue = taskBarClass?.let {
+    catchToNull { taskBarClass.getMethod("setWindowProgressValue", Window::class.java, Integer.TYPE) }
+}
+
+fun setWindowProgressValue(window: Window, value: Int) {
+    catchToNull { setWindowProgressValue?.invoke(taskbar, window, value) }
+}
+
+private inline fun <T> catchToNull(f: () -> T): T? {
+    return try {
+        f()
+    } catch (e: Exception) {
+        null
+    }
+}
