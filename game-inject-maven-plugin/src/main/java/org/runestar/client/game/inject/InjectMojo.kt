5439f0e9f94e18b1cac766e261c4908c60132e87
hunterwb <hunter@hunterwb.com>
2018-01-07 16:37:08 -0800

    Remove stray println

--- game-inject-maven-plugin/src/main/java/org/runestar/client/game/inject/InjectMojo.kt
+++ game-inject-maven-plugin/src/main/java/org/runestar/client/game/inject/InjectMojo.kt
@@ -152,7 +152,6 @@ class InjectMojo : AbstractMojo() {
     }
 
     private fun createMethodProxy(typePool: TypePool, methodHook: MethodHook): Implementation {
-        println(methodHook)
         val sourceMethodDescription = typePool.describe(methodHook.owner).resolve().declaredMethods.first {
             it.name == methodHook.name && it.descriptor == methodHook.descriptor
         }
