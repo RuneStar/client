8b6b67ea321e6f1de8a0aedb5ad176b6c86960f4
hunterwb <hunter@hunterwb.com>
2018-01-12 12:56:32 -0800

    Launcher: use system look and feel

--- launcher/src/main/java/org/runestar/client/launcher/Main.kt
+++ launcher/src/main/java/org/runestar/client/launcher/Main.kt
@@ -20,7 +20,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
-import javax.swing.JOptionPane
+import javax.swing.UIManager
 
 private val locator = MavenRepositorySystemUtils.newServiceLocator().apply {
     addService(TransporterFactory::class.java, FileTransporterFactory::class.java)
@@ -49,6 +49,12 @@ private val remoteRepo =
                 .build()
 
 fun main(args: Array<String>) {
+    try {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
     val frame = LaunchFrame()
     session.transferListener = frame
     try {
