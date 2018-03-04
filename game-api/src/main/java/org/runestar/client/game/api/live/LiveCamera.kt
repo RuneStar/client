3408213edc3ba1f91f2634eac3ab6f98687b40d6
hunterwb <hunter@hunterwb.com>
2018-03-04 11:23:37 -0800

    Fake camera location when sometimes out of bounds

--- game-api/src/main/java/org/runestar/client/game/api/live/LiveCamera.kt
+++ game-api/src/main/java/org/runestar/client/game/api/live/LiveCamera.kt
@@ -3,12 +3,18 @@ package org.runestar.client.game.api.live
 import org.runestar.client.game.api.Angle
 import org.runestar.client.game.api.Camera
 import org.runestar.client.game.api.Position
+import org.runestar.client.game.api.Scene
 import org.runestar.client.game.raw.Client.accessor
 
 object LiveCamera : Camera {
 
-    override val position get() = Position(accessor.cameraX, accessor.cameraZ, accessor.cameraY, accessor.plane).run {
-        copy(height = LiveScene.getTileHeight(this) - height)
+    override val position: Position get() {
+        val l = Position(accessor.cameraX, accessor.cameraZ, accessor.cameraY, accessor.plane)
+        if (!l.isLoaded) {
+            return Scene.CENTER.center // todo
+        }
+        val th = LiveScene.getTileHeight(l)
+        return l.copy(height = LiveScene.getTileHeight(l) - th)
     }
 
     override val pitch get() = Angle(accessor.cameraPitch)
