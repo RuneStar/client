a1f4ae726459a4f8e90ccd2e4070eb25307f3194
hunterwb <hunter@hunterwb.com>
2018-03-20 21:15:11 -0700

    Add SpotAnimationId class

---
+++ game-api/src/main/java/org/runestar/client/game/api/SpotAnimationId.kt
@@ -0,0 +1,12 @@
+package org.runestar.client.game.api
+
+object SpotAnimationId {
+
+    const val ENTANGLE = 179
+    const val SNARE = 180
+    const val BIND = 181
+    const val ICE_RUSH = 361
+    const val ICE_BURST = 363
+    const val ICE_BLITZ = 367
+    const val ICE_BARRAGE = 369
+}
--- plugins-standard/src/main/java/org/runestar/client/plugins/std/freezetimers/FreezeType.kt
+++ plugins-standard/src/main/java/org/runestar/client/plugins/std/freezetimers/FreezeType.kt
@@ -1,18 +1,20 @@
 package org.runestar.client.plugins.std.freezetimers
 
+import org.runestar.client.game.api.SpotAnimationId
+
 enum class FreezeType(
         val ticks: Int,
         val spotAnimation: Int,
         val halfOnPray: Boolean
 ) {
-    ICE_RUSH(8, 361, false),
-    ICE_BURST(16, 363, false),
-    ICE_BLITZ(24, 367, false),
-    ICE_BARRAGE(32, 369, false),
+    ICE_RUSH(8, SpotAnimationId.ICE_RUSH, false),
+    ICE_BURST(16, SpotAnimationId.ICE_BURST, false),
+    ICE_BLITZ(24, SpotAnimationId.ICE_BLITZ, false),
+    ICE_BARRAGE(32, SpotAnimationId.ICE_BARRAGE, false),
 
-    BIND(8, 181, true),
-    SNARE(16, 180, true),
-    ENTANGLE(24, 179, true);
+    BIND(8, SpotAnimationId.BIND, true),
+    SNARE(16, SpotAnimationId.SNARE, true),
+    ENTANGLE(24, SpotAnimationId.ENTANGLE, true);
 
     companion object {
 
