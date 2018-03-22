24f3a3516cd3455c62e6d81875b00ad481fea932
hunterwb <hunter@hunterwb.com>
2018-03-21 18:04:15 -0700

    FreezeTimers: More special cases

--- plugins-standard/src/main/java/org/runestar/client/plugins/std/freezetimers/FreezeTimers.kt
+++ plugins-standard/src/main/java/org/runestar/client/plugins/std/freezetimers/FreezeTimers.kt
@@ -1,8 +1,6 @@
 package org.runestar.client.plugins.std.freezetimers
 
-import org.runestar.client.game.api.Actor
-import org.runestar.client.game.api.HeadIconPrayer
-import org.runestar.client.game.api.Player
+import org.runestar.client.game.api.*
 import org.runestar.client.game.api.live.Game
 import org.runestar.client.game.api.live.LiveCanvas
 import org.runestar.client.game.api.live.Npcs
@@ -12,6 +10,7 @@ import org.runestar.client.utils.DisposablePlugin
 import org.runestar.client.utils.drawStringShadowed
 import org.runestar.general.fonts.RUNESCAPE_SMALL_FONT
 import java.awt.Color
+import java.awt.Graphics2D
 import java.util.concurrent.ConcurrentHashMap
 
 class FreezeTimers : DisposablePlugin<PluginSettings>() {
@@ -20,49 +19,87 @@ class FreezeTimers : DisposablePlugin<PluginSettings>() {
 
     override val name: String = "Freeze Timers"
 
-    private val freezes = ConcurrentHashMap<Actor, FreezeState>() // todo: threading
+    private val playerFreezes = ConcurrentHashMap<String, FreezeState>()
+
+    private val npcFreezes = ConcurrentHashMap<Npc, FreezeState>()
 
     override fun start() {
         super.start()
 
         add(Game.ticks.subscribe {
 
-            tickFreezes()
+            tickFreezes(npcFreezes)
+            tickFreezes(playerFreezes)
 
             for (npc in Npcs) {
-                processActor(npc)
+                processActor(npc, npc, npcFreezes)
             }
 
             for (player in Players) {
-                processActor(player)
+                val name = player.name?.name ?: continue
+                processActor(player, name, playerFreezes)
             }
         })
 
         add(LiveCanvas.repaints.subscribe { g ->
             g.font = RUNESCAPE_SMALL_FONT
-            freezes.forEach { actor, freezeState ->
-                if (!actor.accessor.isVisible) return@forEach
-                val pos = actor.position
-                if (!pos.isLoaded) return@forEach
-                val height = actor.accessor.defaultHeight * 2 / 3
-                val pt = pos.copy(height = height).toScreen() ?: return@forEach
-                g.color = when (freezeState) {
-                    is FreezeState.Frozen -> Color.CYAN
-                    is FreezeState.Immune -> Color.PINK
-                }
-                val s = freezeState.ticksRemaining.toString()
-                g.drawStringShadowed(s, pt.x, pt.y)
+
+            for (npc in Npcs) {
+                val freezeState = npcFreezes[npc] ?: continue
+                drawActor(g, npc, freezeState)
+            }
+
+            for (player in Players) {
+                val name = player.name?.name ?: continue
+                val freezeState = playerFreezes[name] ?: continue
+                drawActor(g, player, freezeState)
             }
         })
     }
 
-    private fun processActor(actor: Actor) {
-        val existingFreeze = freezes[actor]
-        if (existingFreeze == null) {
-            val freezeType = FreezeType.fromSpotAnimation(actor.accessor.spotAnimation)
-            if (freezeType != null) {
-                freezes[actor] = freezeStateForActor(actor, freezeType)
+    private fun drawActor(
+            g: Graphics2D,
+            actor: Actor,
+            freezeState: FreezeState
+    ) {
+        val pos = actor.position
+        if (!pos.isLoaded) return
+        val height = actor.accessor.defaultHeight * 2 / 3
+        val pt = pos.copy(height = height).toScreen() ?: return
+        g.color = when (freezeState) {
+            is FreezeState.Frozen -> Color.CYAN
+            is FreezeState.Immune -> Color.PINK
+        }
+        val s = freezeState.ticksRemaining.toString()
+        g.drawStringShadowed(s, pt.x, pt.y)
+    }
+
+    private fun <T> processActor(
+            actor: Actor,
+            key: T,
+            freezes: MutableMap<T, FreezeState>
+    ) {
+        if (actor.accessor.walkSequence == -1 && actor.accessor.runSequence == -1) {
+            // npc which can't move, like a castle wars barricade
+            return
+        }
+        if (actor.accessor.sequence == SequenceId.HUMAN_DYING) {
+            freezes.remove(key)
+            return
+        }
+        val existingFreeze = freezes[key]
+        if (existingFreeze != null) {
+            if (existingFreeze is FreezeState.Frozen) {
+                val mvmt = actor.accessor.movementSequence
+                if (mvmt == actor.accessor.walkSequence || mvmt == actor.accessor.runSequence) {
+                    freezes.remove(key)
+                }
             }
+            return
+        }
+        val freezeType = FreezeType.fromSpotAnimation(actor.accessor.spotAnimation)
+        if (freezeType != null) {
+            freezes[key] = freezeStateForActor(actor, freezeType)
         }
     }
 
@@ -78,7 +115,7 @@ class FreezeTimers : DisposablePlugin<PluginSettings>() {
         return FreezeState.Frozen(ticks)
     }
 
-    private fun tickFreezes() {
+    private fun tickFreezes(freezes: MutableMap<*, FreezeState>) {
         val itr = freezes.iterator()
         while (itr.hasNext()) {
             val e = itr.next()
