3e67432f93034531039ff0e71efdc5b78c355eed
hunterwb <hunter@hunterwb.com>
2018-03-21 18:02:40 -0700

    AnimationDebug: draw all actor animations

--- plugins-standard/src/main/java/org/runestar/client/plugins/std/debug/AnimationDebug.kt
+++ plugins-standard/src/main/java/org/runestar/client/plugins/std/debug/AnimationDebug.kt
@@ -1,11 +1,15 @@
 package org.runestar.client.plugins.std.debug
 
+import org.runestar.client.game.api.Actor
 import org.runestar.client.game.api.live.LiveCanvas
+import org.runestar.client.game.api.live.Npcs
 import org.runestar.client.game.api.live.Players
 import org.runestar.client.plugins.PluginSettings
 import org.runestar.client.utils.DisposablePlugin
+import org.runestar.client.utils.drawStringShadowed
 import org.runestar.general.fonts.RUNESCAPE_CHAT_FONT
 import java.awt.Color
+import java.awt.Graphics2D
 
 class AnimationDebug : DisposablePlugin<PluginSettings>() {
 
@@ -34,6 +38,23 @@ class AnimationDebug : DisposablePlugin<PluginSettings>() {
                 g.drawString(s, x, y)
                 y += 25
             }
+
+            for (a in Npcs) {
+                drawActor(g, a)
+            }
+            for (a in Players) {
+                drawActor(g, a)
+            }
         })
+    }
+
+    private fun drawActor(g: Graphics2D, actor: Actor) {
+        val pos = actor.position
+        if (!pos.isLoaded) return
+        val height = actor.accessor.defaultHeight * 2 / 3
+        val pt = pos.copy(height = height).toScreen() ?: return
+        val s = "sa=${actor.accessor.spotAnimation}," +
+                "sq=${actor.accessor.sequence}"
+        g.drawStringShadowed(s, pt.x, pt.y)
     }
 }
