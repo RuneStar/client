8f91ff50d6b7414f56aa393ad4ffc692a1648d79
hunterwb <hunter@hunterwb.com>
2018-03-25 18:23:39 -0700

    Safer Projectiles iteration

--- game-api/src/main/java/org/runestar/client/game/api/live/Projectiles.kt
+++ game-api/src/main/java/org/runestar/client/game/api/live/Projectiles.kt
@@ -2,15 +2,17 @@ package org.runestar.client.game.api.live
 
 import org.runestar.client.game.api.Projectile
 import org.runestar.client.game.raw.Client.accessor
+import org.runestar.client.game.raw.access.XNode
 import org.runestar.client.game.raw.access.XProjectile
 
 object Projectiles : Iterable<Projectile> {
 
     override fun iterator() = object : Iterator<Projectile> {
+
         private var n = accessor.projectiles.sentinel.previous
-        override fun hasNext() = n != accessor.projectiles.sentinel
+
+        override fun hasNext() = n != null && n != accessor.projectiles.sentinel
+
         override fun next() = Projectile(n as XProjectile).also { n = n.previous }
     }
-
-    fun isEmpty() = accessor.projectiles.sentinel.previous == accessor.projectiles.sentinel
 }
