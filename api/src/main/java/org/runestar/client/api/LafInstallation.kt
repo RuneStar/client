1c8783044fa547b3421bc0bed7e920037d5cabcd
hunterwb <hunter@hunterwb.com>
2018-04-12 06:11:45 -0700

    Set RS font to default for all components

--- api/src/main/java/org/runestar/client/api/LafInstallation.kt
+++ api/src/main/java/org/runestar/client/api/LafInstallation.kt
@@ -4,7 +4,6 @@ import com.alee.laf.WebLookAndFeel
 import com.alee.skin.dark.DarkSkin
 import org.runestar.general.fonts.RUNESCAPE_CHAT_FONT
 import javax.swing.JPopupMenu
-import javax.swing.PopupFactory
 import javax.swing.ToolTipManager
 import javax.swing.UIManager
 import javax.swing.plaf.FontUIResource
@@ -12,18 +11,17 @@ import javax.swing.plaf.FontUIResource
 internal object LafInstallation : Runnable {
 
     override fun run() {
-        val chatFont = FontUIResource(RUNESCAPE_CHAT_FONT)
-
-        UIManager.put("Label.font", chatFont)
-        UIManager.put("Button.font", chatFont)
-        UIManager.put("ToggleButton.font", chatFont)
-        UIManager.put("Tree.font", chatFont)
-        UIManager.put("ComboBox.font", chatFont)
-        UIManager.put("MenuItem.font", chatFont)
-
         WebLookAndFeel.install(DarkSkin::class.java)
 
         JPopupMenu.setDefaultLightWeightPopupEnabled(false)
         ToolTipManager.sharedInstance().isLightWeightPopupEnabled = false
+
+        val chatFont = FontUIResource(RUNESCAPE_CHAT_FONT)
+        UIManager.getDefaults().entries.forEach { e ->
+            val k = e.key as? String ?: return@forEach
+            if (k.endsWith(".font")) {
+                e.setValue(chatFont)
+            }
+        }
     }
 }
