49474da2acc01fb7638fbc7b5233e5664b49645a
hunterwb <hunter@hunterwb.com>
2018-04-19 15:00:29 -0700

    Add SceneTile.isVisible()

--- plugins-dev/src/main/java/org/runestar/client/plugins/dev/ObjectClickBoxDebug.kt
+++ plugins-dev/src/main/java/org/runestar/client/plugins/dev/ObjectClickBoxDebug.kt
@@ -1,10 +1,7 @@
 package org.runestar.client.plugins.dev
 
 import org.runestar.client.game.api.SceneObject
-import org.runestar.client.game.api.live.Game
-import org.runestar.client.game.api.live.LiveCanvas
-import org.runestar.client.game.api.live.LiveViewport
-import org.runestar.client.game.api.live.SceneObjects
+import org.runestar.client.game.api.live.*
 import org.runestar.client.game.raw.access.XScene
 import org.runestar.client.plugins.spi.PluginSettings
 import org.runestar.client.utils.DisposablePlugin
@@ -25,11 +22,12 @@ class ObjectClickBoxDebug : DisposablePlugin<PluginSettings>() {
         add(LiveCanvas.repaints.subscribe { g ->
             g.color = color
             val viewport = LiveViewport.shape
+            val cam = LiveCamera.position.sceneTile
             objs.forEach {
                 val loc = it.location
-                if (loc.plane != Game.plane) return@forEach
-                val center = loc.center.toScreen() ?: return@forEach
-                if (center !in viewport) return@forEach
+                if (loc.plane != Game.plane || !loc.isVisible(cam)) return@forEach
+                val pt = loc.center.toScreen() ?: return@forEach
+                if (pt !in viewport) return@forEach
                 it.models.forEach {
                     g.fill(it.objectClickBoxOutline())
                 }
