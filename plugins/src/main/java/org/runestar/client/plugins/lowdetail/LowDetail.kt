5c37098ac4545d2c984bf18ff7ff9b1ad000275f
hunterwb <hunter@hunterwb.com>
2018-04-27 18:00:18 -0700

    Low detail: do not change audio

--- plugins/src/main/java/org/runestar/client/plugins/lowdetail/LowDetail.kt
+++ plugins/src/main/java/org/runestar/client/plugins/lowdetail/LowDetail.kt
@@ -10,16 +10,15 @@ class LowDetail : AbstractPlugin<PluginSettings>() {
 
     override val name = "Low Detail"
 
-    override fun start() {Client.accessor.isLowDetail = true
+    override fun start() {
+        Client.accessor.isLowDetail = true
         Client.accessor.objectDefinition_isLowDetail = true
         Client.accessor.scene_isLowDetail = true
-        Client.accessor.isStereo = false
     }
 
     override fun stop() {
         Client.accessor.isLowDetail = false
         Client.accessor.objectDefinition_isLowDetail = false
         Client.accessor.scene_isLowDetail = false
-        Client.accessor.isStereo = true
     }
 }
