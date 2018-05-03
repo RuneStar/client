ae13e1fbe9845d298ccd096aceb9c51dce13c362
hunterwb <hunter@hunterwb.com>
2018-05-02 18:55:52 -0700

    Start HideLowerFloors plugin

---
+++ plugins/src/main/java/org/runestar/client/plugins/hidelowerfloors/HideLowerFloors.kt
@@ -0,0 +1,22 @@
+package org.runestar.client.plugins.hidelowerfloors
+
+import org.runestar.client.game.api.live.Game
+import org.runestar.client.game.raw.Client
+import org.runestar.client.plugins.spi.PluginSettings
+import org.runestar.client.utils.DisposablePlugin
+
+class HideLowerFloors : DisposablePlugin<PluginSettings>() {
+
+    override val defaultSettings = PluginSettings()
+
+    override val name = "Hide Lower Floors"
+
+    override fun start() {
+        add(Game.ticks.subscribe { Client.accessor.scene.minPlane = Game.plane })
+    }
+
+    override fun stop() {
+        super.stop()
+        Client.accessor.scene.minPlane = 0
+    }
+}
