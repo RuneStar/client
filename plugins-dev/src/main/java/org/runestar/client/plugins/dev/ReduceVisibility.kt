64d780ba78bb123b99827f24e6e4ee56e40e910a
hunterwb <hunter@hunterwb.com>
2018-05-22 01:47:02 -0700

    Add reduce visibility plugin

---
+++ plugins-dev/src/main/java/org/runestar/client/plugins/dev/ReduceVisibility.kt
@@ -0,0 +1,41 @@
+package org.runestar.client.plugins.dev
+
+import org.runestar.client.api.util.DisposablePlugin
+import org.runestar.client.game.raw.Client
+import org.runestar.client.game.raw.access.XClient
+import org.runestar.client.plugins.spi.PluginSettings
+
+class ReduceVisibility : DisposablePlugin<ReduceVisibility.Settings>() {
+
+    override val defaultSettings = Settings()
+
+    override fun start() {
+        adjustVisibilityMap()
+        add(XClient.Scene_buildVisiblityMap.exit.subscribe { adjustVisibilityMap() })
+    }
+
+    private fun adjustVisibilityMap() {
+        val amt = ctx.settings.amount
+        val vm = Client.accessor.visibilityMap
+        for (pitch in 0 until vm.size) {
+            val atPitch = vm[pitch]
+            for (yaw in 0 until atPitch.size) {
+                val atYaw = atPitch[yaw]
+                val maxX = atYaw.size - 1
+                for (x in 0..maxX) {
+                    val atX = atYaw[x]
+                    val maxY = atX.size - 1
+                    for (y in 0..maxY) {
+                        if (x - amt < 0 || x + amt > maxX || y - amt < 0 || y + amt > maxY) {
+                            atX[y] = false
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    class Settings(
+            val amount: Int = 10
+    ) : PluginSettings()
+}
