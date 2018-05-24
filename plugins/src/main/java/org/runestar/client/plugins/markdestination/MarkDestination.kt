cf9c878b626817a5dbeca1381b3cb1d4751d372a
hunterwb <hunter@hunterwb.com>
2018-05-24 07:24:26 -0700

    Add Mark Destination settings

--- plugins/src/main/java/org/runestar/client/plugins/markdestination/MarkDestination.kt
+++ plugins/src/main/java/org/runestar/client/plugins/markdestination/MarkDestination.kt
@@ -1,31 +1,36 @@
 package org.runestar.client.plugins.markdestination
 
+import org.runestar.client.api.util.DisposablePlugin
+import org.runestar.client.api.util.RgbaForm
 import org.runestar.client.game.api.live.Game
 import org.runestar.client.game.api.live.LiveCanvas
 import org.runestar.client.plugins.spi.PluginSettings
-import org.runestar.client.api.util.DisposablePlugin
 import java.awt.BasicStroke
 import java.awt.Color
 import java.awt.RenderingHints
 
-class MarkDestination : DisposablePlugin<PluginSettings>() {
+class MarkDestination : DisposablePlugin<MarkDestination.Settings>() {
 
-    private companion object {
-        val STROKE = BasicStroke(2.0f)
-    }
-
-    override val defaultSettings = PluginSettings()
+    override val defaultSettings = Settings()
 
     override val name = "Mark Destination"
 
     override fun start() {
+        val stroke = BasicStroke(ctx.settings.stroke)
+        val color = ctx.settings.color.get()
         add(LiveCanvas.repaints.subscribe { g ->
             val destination = Game.destination ?: return@subscribe
+            if (!destination.isLoaded) return@subscribe
             val outline = destination.outline()
             g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-            g.color = Color.GRAY
-            g.stroke = STROKE
+            g.color = color
+            g.stroke = stroke
             g.draw(outline)
         })
     }
+
+    class Settings(
+            val stroke: Float = 2.0f,
+            val color: RgbaForm = RgbaForm(Color.WHITE)
+    ) : PluginSettings()
 }
