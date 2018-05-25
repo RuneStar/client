39a41af21dd3a672e1b1d4393184aa5a6707c438
hunterwb <hunter@hunterwb.com>
2018-05-24 22:07:54 -0700

    Add Keyboard isKeyPressed

--- game-api/src/main/java/org/runestar/client/game/api/live/Keyboard.kt
+++ game-api/src/main/java/org/runestar/client/game/api/live/Keyboard.kt
@@ -2,6 +2,7 @@ package org.runestar.client.game.api.live
 
 import hu.akarnokd.rxjava2.swing.SwingObservable
 import io.reactivex.Observable
+import org.runestar.client.game.raw.Client
 import java.awt.event.KeyEvent
 
 object Keyboard {
@@ -10,4 +11,19 @@ object Keyboard {
      * @see[java.awt.event.KeyListener]
      */
     val events: Observable<KeyEvent> = LiveCanvas.canvasReplacements.flatMap { SwingObservable.keyboard(it) }
+
+    /**
+     * @see[KeyEvent.keyCode]
+     */
+    fun isKeySupported(keyCode: Int): Boolean {
+        return keyCode in Client.accessor.keyHandler_keyCodes.indices && Client.accessor.keyHandler_keyCodes[keyCode] >= 0
+    }
+
+    /**
+     * @see[KeyEvent.keyCode]
+     */
+    fun isKeyPressed(keyCode: Int): Boolean {
+        require(isKeySupported(keyCode)) { "Unsupported KeyCode: $keyCode" }
+        return Client.accessor.keyHandler_pressedKeys[Client.accessor.keyHandler_keyCodes[keyCode]]
+    }
 }
