681cf7989851bd950bde8bca398f40b25baca445
hunterwb <hunter@hunterwb.com>
2018-05-30 18:51:24 -0700

    Add varcs debug plugin

---
+++ plugins-dev/src/main/java/org/runestar/client/plugins/dev/VarcsChangeDebug.kt
@@ -0,0 +1,26 @@
+package org.runestar.client.plugins.dev
+
+import org.kxtra.slf4j.logger.debug
+import org.runestar.client.api.util.DisposablePlugin
+import org.runestar.client.game.raw.access.XVarcs
+import org.runestar.client.plugins.spi.PluginSettings
+
+class VarcsChangeDebug : DisposablePlugin<PluginSettings>() {
+
+    override val defaultSettings = PluginSettings()
+
+    override fun start() {
+        add(XVarcs.setInt.enter.subscribe {
+            val index = it.arguments[0] as Int
+            val newValue = it.arguments[1] as Int
+            val oldValue = it.instance.getInt(index)
+            ctx.logger.debug { "Varcs int $index: $oldValue -> $newValue" }
+        })
+        add(XVarcs.setString.enter.subscribe {
+            val index = it.arguments[0] as Int
+            val newValue = it.arguments[1] as String
+            val oldValue = it.instance.getString(index)
+            ctx.logger.debug { "Varcs str $index: $oldValue -> $newValue" }
+        })
+    }
+}
