8b35a38fbf5c56b19f782e2e8ec3d493d6c8a96c
hunterwb <hunter@hunterwb.com>
2018-06-10 03:21:28 -0700

    Start evicting hash table cache miss plugin

---
+++ plugins-dev/src/main/java/org/runestar/client/plugins/dev/EvictingHashTableDebug.kt
@@ -0,0 +1,25 @@
+package org.runestar.client.plugins.dev
+
+import org.kxtra.slf4j.logger.info
+import org.runestar.client.api.util.DisposablePlugin
+import org.runestar.client.game.raw.access.XEvictingDualNodeHashTable
+import org.runestar.client.plugins.spi.PluginSettings
+
+class EvictingHashTableDebug : DisposablePlugin<PluginSettings>() {
+
+    override val defaultSettings = PluginSettings()
+
+    override fun start() {
+        add(XEvictingDualNodeHashTable.get.exit.filter { it.returned == null && it.instance.remainingCapacity == 0 }.subscribe {
+            val stack = Throwable().stackTrace.asList()
+                    .drop(7)
+                    .dropLast(1)
+                    .filter { !it.methodName.contains('$') }
+                    .map { "${it.className}.${it.methodName}" }
+            val elementType = it.instance.deque.sentinel.nextDual.javaClass.interfaces.firstOrNull()?.simpleName ?: "?"
+            ctx.logger.info {
+                "Cache miss: ${it.arguments[0]} in ${it.instance}(capacity=${it.instance.capacity}, type=$elementType) at $stack"
+            }
+        })
+    }
+}
