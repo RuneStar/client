3da23afc44cc5c1d07befaa4b929e4f7add8939f
hunterwb <hunter@hunterwb.com>
2018-06-15 02:44:42 -0700

    Serialize settings by fields only

--- api/src/main/java/org/runestar/client/api/YamlFileReadWriter.kt
+++ api/src/main/java/org/runestar/client/api/YamlFileReadWriter.kt
@@ -1,5 +1,7 @@
 package org.runestar.client.api
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.PropertyAccessor
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import org.runestar.client.plugins.spi.FileReadWriter
 import java.nio.file.Path
@@ -7,6 +9,8 @@ import java.nio.file.Path
 internal object YamlFileReadWriter : FileReadWriter {
 
     private val mapper = YAMLMapper().findAndRegisterModules()
+            .setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE)
+            .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
 
     override val fileExtension = "yml"
 
