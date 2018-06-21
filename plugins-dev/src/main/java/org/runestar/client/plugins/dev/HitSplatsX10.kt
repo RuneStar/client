e913955066f6e8152caf06cc4034a60dafc628d2
hunterwb <hunter@hunterwb.com>
2018-06-21 12:49:26 -0700

    Add hitsplats x10 plugin

---
+++ plugins-dev/src/main/java/org/runestar/client/plugins/dev/HitSplatsX10.kt
@@ -0,0 +1,18 @@
+package org.runestar.client.plugins.dev
+
+import org.runestar.client.api.util.DisposablePlugin
+import org.runestar.client.game.raw.access.XHitSplatDefinition
+import org.runestar.client.plugins.spi.PluginSettings
+
+class HitSplatsX10 : DisposablePlugin<PluginSettings>() {
+
+    override val defaultSettings = PluginSettings()
+
+    override fun start() {
+        add(XHitSplatDefinition.getString.exit.subscribe {
+            if (it.returned != "0") {
+                it.returned += '0'
+            }
+        })
+    }
+}
