64d177fbbafaae63de6686192db7a05a76d1b623
hunterwb <hunter@hunterwb.com>
2018-07-10 15:27:32 -0700

    Simplify inversion

--- updater-common/src/main/java/org/runestar/client/updater/common/Inversion.kt
+++ updater-common/src/main/java/org/runestar/client/updater/common/Inversion.kt
@@ -6,18 +6,25 @@ private val UNSIGNED_INT_MODULUS = BigInteger.ONE.shiftLeft(Integer.SIZE)
 
 private val UNSIGNED_LONG_MODULUS = BigInteger.ONE.shiftLeft(java.lang.Long.SIZE)
 
+fun invert(n: Int): Int = n.toBigInteger().modInverse(UNSIGNED_INT_MODULUS).toInt()
+
+fun invert(n: Long): Long = n.toBigInteger().modInverse(UNSIGNED_LONG_MODULUS).toLong()
+
 fun invert(n: Number): Number {
     return when (n) {
-        is Int -> BigInteger.valueOf(n.toLong()).modInverse(UNSIGNED_INT_MODULUS).toInt()
-        is Long -> BigInteger.valueOf(n).modInverse(UNSIGNED_LONG_MODULUS).toLong()
+        is Int -> invert(n)
+        is Long -> invert(n)
         else -> error(n)
     }
 }
 
+fun isInvertible(n: Int): Boolean = n and 1 == 1
+
+fun isInvertible(n: Long): Boolean = isInvertible(n.toInt())
+
 fun isInvertible(n: Number): Boolean {
     return when (n) {
-        is Int -> BigInteger.valueOf(n.toLong()).gcd(UNSIGNED_INT_MODULUS) == BigInteger.ONE
-        is Long -> BigInteger.valueOf(n).gcd(UNSIGNED_LONG_MODULUS) == BigInteger.ONE
+        is Int, is Long -> isInvertible(n.toInt())
         else -> error(n)
     }
 }
