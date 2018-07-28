0f0deccd7ed702b3d5250b7e7773bcd543e7a2d8
hunterwb <hunter@hunterwb.com>
2018-07-27 21:43:24 -0700

    Pin Overhead Text: Add auto-chat only setting

--- plugins/src/main/java/org/runestar/client/plugins/pinoverheadtext/PinOverheadText.kt
+++ plugins/src/main/java/org/runestar/client/plugins/pinoverheadtext/PinOverheadText.kt
@@ -5,19 +5,26 @@ import org.runestar.client.game.api.live.Players
 import org.runestar.client.game.raw.access.XClient
 import org.runestar.client.plugins.spi.PluginSettings
 
-class PinOverheadText : DisposablePlugin<PluginSettings>() {
+class PinOverheadText : DisposablePlugin<PinOverheadText.Settings>() {
 
     override val name = "Pin Overhead Text"
 
-    override val defaultSettings = PluginSettings()
+    override val defaultSettings = Settings()
 
     override fun start() {
         add(XClient.doCycle.exit.subscribe {
             Players.forEach { player ->
-                if (player.overheadTextCyclesRemaining > 0) {
-                    player.overheadTextCyclesRemaining = 2
+                if (!settings.autoChatOnly || player.accessor.isAutoChatting) {
+                    if (player.overheadTextCyclesRemaining > 0) {
+                        player.overheadTextCyclesRemaining = 2
+                    }
                 }
+
             }
         })
     }
+
+    class Settings(
+            val autoChatOnly: Boolean = true
+    ) : PluginSettings()
 }
