7bcf70386e3fc10d04a2707b4025287c9bb39e03
hunterwb <hunter@hunterwb.com>
2018-08-02 18:58:57 -0700

    Start World History plugin

---
+++ plugins/src/main/java/org/runestar/client/plugins/worldhistory/WorldHistory.kt
@@ -0,0 +1,31 @@
+package org.runestar.client.plugins.worldhistory
+
+import com.google.common.collect.EvictingQueue
+import org.runestar.client.api.util.DisposablePlugin
+import org.runestar.client.game.api.GameState
+import org.runestar.client.game.api.live.Game
+import org.runestar.client.game.raw.CLIENT
+import org.runestar.client.plugins.spi.PluginSettings
+import java.util.*
+
+class WorldHistory : DisposablePlugin<PluginSettings>() {
+
+    override val defaultSettings = PluginSettings()
+
+    override val name = "World History"
+
+    private val history: Queue<Int> = EvictingQueue.create(14)
+
+    override fun start() {
+        add(Game.stateChanges.filter { it == GameState.LOGGED_IN }.subscribe {
+            history.add(CLIENT.worldId - 300)
+            val msg = "World History: ${history.reversed().joinToString()}"
+            CLIENT.addMessage(0, "", msg, null)
+        })
+    }
+
+    override fun stop() {
+        super.stop()
+        history.clear()
+    }
+}
