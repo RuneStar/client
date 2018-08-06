558d9e98cdf2a5c18054340246e4070827c3146a
hunterwb <hunter@hunterwb.com>
2018-08-06 16:01:01 -0700

    Add FontId

---
+++ game-api/src/main/java/org/runestar/client/game/api/FontId.java
@@ -0,0 +1,12 @@
+package org.runestar.client.game.api;
+
+public final class FontId {
+
+    private FontId() {}
+
+    public static int PLAIN_11 = 494;
+    public static int PLAIN_12 = 495;
+    public static int BOLD_12 = 496;
+    public static int QUILL_8 = 497;
+    public static int QUILL = 645;
+}
