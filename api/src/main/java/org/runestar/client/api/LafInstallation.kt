7535c9b527b97445e0673564708d8fbd3a5c006c
hunterwb <hunter@hunterwb.com>
2018-08-22 01:52:47 -0700

    Disable all anti-aliasing

--- api/src/main/java/org/runestar/client/api/LafInstallation.kt
+++ api/src/main/java/org/runestar/client/api/LafInstallation.kt
@@ -1,7 +1,9 @@
 package org.runestar.client.api
 
 import com.alee.laf.WebLookAndFeel
+import com.alee.painter.decoration.content.TextRasterization
 import com.alee.skin.dark.DarkSkin
+import java.awt.RenderingHints
 import javax.swing.JPopupMenu
 import javax.swing.ToolTipManager
 import javax.swing.UIManager
@@ -15,14 +17,21 @@ class LafInstallation : Runnable {
         JPopupMenu.setDefaultLightWeightPopupEnabled(false)
         ToolTipManager.sharedInstance().isLightWeightPopupEnabled = false
 
-        val plain12Resource = FontUIResource(Fonts.PLAIN_12)
         val uiDefaults = UIManager.getDefaults()
+
+        val plain12Resource = FontUIResource(Fonts.PLAIN_12)
         for (key in uiDefaults.keys()) {
             val prevFont = uiDefaults.getFont(key)
             if (prevFont != null) {
                 uiDefaults[key] = plain12Resource
             }
         }
+
+        uiDefaults.remove(RenderingHints.KEY_TEXT_ANTIALIASING)
+        uiDefaults.remove(RenderingHints.KEY_TEXT_LCD_CONTRAST)
+
+        TextRasterization.basic.renderingHints.clear()
+        TextRasterization.subpixel.renderingHints.clear()
 
         System.setProperty("sun.awt.noerasebackground", "true")
     }
