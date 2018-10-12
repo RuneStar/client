4a9e69e2cf082f556c5041674ab1637a0c0953a6
hunterwb <hunter@hunterwb.com>
2018-10-12 16:09:26 -0700

    Add Brightness plugin

--- game-api/src/main/java/org/runestar/client/game/api/VarpId.java
+++ game-api/src/main/java/org/runestar/client/game/api/VarpId.java
@@ -6,6 +6,8 @@ public final class VarpId {
 
     public static final int ATTACK_STYLE = 43;
 
+    public static final int BRIGHTNESS = 166;
+
     public static final int SPECIAL_ATTACK_PERCENT = 300;
     public static final int SPECIAL_ATTACK_ENABLED = 301;
 
---
+++ plugins/src/main/java/org/runestar/client/plugins/brightness/Brightness.kt
@@ -0,0 +1,36 @@
+package org.runestar.client.plugins.brightness
+
+import io.reactivex.rxkotlin.subscribeBy
+import org.runestar.client.game.api.GameState
+import org.runestar.client.game.api.VarpId
+import org.runestar.client.game.api.live.Game
+import org.runestar.client.game.raw.CLIENT
+import org.runestar.client.game.raw.access.XClient
+import org.runestar.client.game.raw.access.XTextureProvider
+import org.runestar.client.plugins.spi.AbstractPlugin
+import org.runestar.client.plugins.spi.PluginSettings
+
+class Brightness : AbstractPlugin<Brightness.Settings>() {
+
+    override val defaultSettings = Settings()
+
+    override fun start() {
+        setBrightness(settings.brightness)
+    }
+
+    override fun stop() {
+        setBrightness(1.0 - Game.varps[VarpId.BRIGHTNESS] * 0.1)
+    }
+
+    private fun setBrightness(brightness: Double) {
+        XClient.doCycle.enter.any { Game.state == GameState.LOGGED_IN }.subscribeBy {
+            CLIENT.sprite_cached.clear()
+            CLIENT.Rasterizer3D_setBrightness(brightness)
+            (CLIENT.rasterizer3D_textureLoader as XTextureProvider).setBrightness(brightness)
+        }
+    }
+
+    class Settings(
+            val brightness: Double = 0.5
+    ) : PluginSettings()
+}
