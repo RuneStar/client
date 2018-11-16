80227c19893db8d09f399a3f90c4b6869c0d8c6c
hunterwb <hunter@hunterwb.com>
2018-11-15 16:22:35 -0800

    Check server revision with jav_config

--- client/src/main/java/org/runestar/client/Main.kt
+++ client/src/main/java/org/runestar/client/Main.kt
@@ -10,18 +10,13 @@ import org.runestar.client.api.Application
 import org.runestar.client.api.LafInstallation
 import org.runestar.client.common.JAV_CONFIG
 import org.runestar.client.common.REVISION
-import java.io.IOException
-import java.net.InetSocketAddress
-import java.net.Socket
-import java.net.SocketAddress
-import java.nio.ByteBuffer
 import javax.swing.SwingUtilities
 
 fun main(args: Array<String>) {
     setupLogging()
     SwingUtilities.invokeLater(LafInstallation())
 
-    if (!isUpToDate()) throw Exception("Client is out of date")
+    if (JAV_CONFIG.revision != REVISION) throw Exception("Client is out of date")
 
     Application.start()
 }
@@ -33,21 +28,5 @@ private fun setupLogging() {
     }
     Thread.setDefaultUncaughtExceptionHandler { t, e ->
         logger.error(e) { "Uncaught exception for thread ${t.name}" }
-    }
-}
-
-@Throws(IOException::class)
-private fun isUpToDate(): Boolean {
-    val address = InetSocketAddress(JAV_CONFIG.codebase.host, 43594)
-    return checkServerRevision(address, REVISION)
-}
-
-@Throws(IOException::class)
-private fun checkServerRevision(address: SocketAddress, revision: Int): Boolean {
-    Socket().use { socket ->
-        socket.connect(address)
-        socket.outputStream.write(ByteBuffer.allocate(5).put(15).putInt(revision).array())
-        socket.outputStream.flush()
-        return socket.inputStream.read() == 0
     }
 }
