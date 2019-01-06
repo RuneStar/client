f8a87c31bee8698f823dcec6aa4c2746bae2b75e
hunterwb <hunter@hunterwb.com>
2019-01-06 02:45:19 -0800

    Less AWT logging

--- api/src/main/java/org/runestar/client/api/util/AwtSystem.kt
+++ api/src/main/java/org/runestar/client/api/util/AwtSystem.kt
@@ -2,9 +2,8 @@
 
 package org.runestar.client.api.util
 
+import org.kxtra.slf4j.debug
 import org.kxtra.slf4j.getLogger
-import org.kxtra.slf4j.info
-import org.kxtra.slf4j.warn
 import java.awt.*
 import java.nio.file.Files
 import java.nio.file.Path
@@ -15,7 +14,7 @@ private inline fun <T> safe(f: () -> T): T? {
     return try {
         f()
     } catch (e: Exception) {
-        logger.warn(e) { "" }
+        logger.debug(e.message)
         null
     }
 }
@@ -51,15 +50,15 @@ fun Desktop.safeOpen(path: Path) {
         try {
             open(path.toFile())
         } catch (e: Exception) {
-            logger.warn(e) { "Desktop failed to open $path" }
+            logger.debug { "Desktop failed to open $path" }
             if (Files.isRegularFile(path)) {
                 val dir = path.parent
-                logger.info { "Desktop opening parent directory $dir" }
+                logger.debug { "Desktop opening parent directory $dir" }
                 safeOpen(dir)
             }
         }
     } else {
-        logger.warn { "Desktop open is not supported" }
+        logger.debug { "Desktop open is not supported" }
     }
 }
 
