51d4804635a8123383277385846b1a04e0380bbf
hunterwb <hunter@hunterwb.com>
2019-01-15 02:03:57 -0800

    Cleanup Clock plugin

--- plugins/src/main/java/org/runestar/client/plugins/clock/Clock.kt
+++ plugins/src/main/java/org/runestar/client/plugins/clock/Clock.kt
@@ -1,6 +1,7 @@
 package org.runestar.client.plugins.clock
 
 import com.google.common.base.Stopwatch
+import org.runestar.client.api.forms.DateTimeFormatterForm
 import org.runestar.client.api.util.DisposablePlugin
 import org.runestar.client.game.api.Widget
 import org.runestar.client.game.api.WidgetId
@@ -9,24 +10,12 @@ import org.runestar.client.game.api.live.Worlds
 import org.runestar.client.game.raw.access.XClient
 import org.runestar.client.plugins.spi.PluginSettings
 import java.time.Duration
-import java.time.LocalTime
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
-import java.time.Clock as Clock8
+import java.time.Instant
 
 class Clock : DisposablePlugin<Clock.Settings>() {
 
     private companion object {
         const val DEFAULT_TEXT = "Report"
-
-        val UTC_CLOCK: Clock8 = Clock8.systemUTC()
-        val LOCAL_CLOCK: Clock8 = Clock8.systemDefaultZone()
-        val JAGEX_CLOCK: Clock8 = Clock8.system(ZoneId.of("Europe/London"))
-
-        val TIME_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)
-
-        fun timeNow(clock: Clock8): String = LocalTime.now(clock).format(TIME_FORMATTER)
     }
 
     override val defaultSettings = Settings()
@@ -35,23 +24,20 @@ class Clock : DisposablePlugin<Clock.Settings>() {
 
     override fun onStart() {
         add(XClient.drawWidgetsLoggedIn.enter.subscribe {
-            val w = getReportWidget() ?: return@subscribe
-            w.text = when (settings.style) {
-                Style.UTC -> timeNow(UTC_CLOCK)
-                Style.LOCAL -> timeNow(LOCAL_CLOCK)
-                Style.JAGEX -> timeNow(JAGEX_CLOCK)
-                Style.LOGIN -> loginElapsedTime() ?: DEFAULT_TEXT
+            getReportWidget()?.text = if (settings.loginTime) {
+                loginElapsedTime() ?: DEFAULT_TEXT
+            } else {
+                settings.dateTimeFormatter.get().format(Instant.now())
             }
         })
-        if (settings.style == Style.LOGIN) {
+        if (settings.loginTime) {
             add(Worlds.enter.subscribe { loginTimer.reset().start() })
         }
     }
 
     override fun onStop() {
         loginTimer.reset()
-        val w = getReportWidget() ?: return
-        w.text = DEFAULT_TEXT
+        getReportWidget()?.text = DEFAULT_TEXT
     }
 
     private fun getReportWidget(): Widget.Text? = Widgets[WidgetId.CHAT_REPORT_TEXT] as Widget.Text?
@@ -60,16 +46,13 @@ class Clock : DisposablePlugin<Clock.Settings>() {
         val duration = loginTimer.elapsed()
         if (duration == Duration.ZERO) return null
         val hrs = duration.toHours()
-        val mins = duration.toMinutes() % 60
-        val seconds = duration.seconds % 60
+        val mins = duration.toMinutesPart()
+        val seconds = duration.toSecondsPart()
         return "%d:%02d:%02d".format(hrs, mins, seconds)
     }
 
     class Settings(
-            val style: Style = Style.LOGIN
+            val loginTime: Boolean = true,
+            val dateTimeFormatter: DateTimeFormatterForm = DateTimeFormatterForm("hh:mm:ss", null)
     ) : PluginSettings()
-
-    enum class Style {
-        UTC, LOCAL, JAGEX, LOGIN;
-    }
 }
--- plugins/src/main/java/org/runestar/client/plugins/clock/README.md
+++ plugins/src/main/java/org/runestar/client/plugins/clock/README.md
@@ -1,12 +1,3 @@
 # Clock
 
-Replaces the text of the Report Abuse button with a clock
-
-Styles:
-
-| Name | Description |
-| --- | --- |
-| UTC | Current time using UTC |
-| Local | Current time using the local time zone |
-| Jagex | Current time using GMT/BST |
-| Login | Time since login |
+Replaces the text of the Report Abuse button with a clock or the time since login
