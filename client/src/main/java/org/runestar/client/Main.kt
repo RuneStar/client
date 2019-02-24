00a12e9a60b1e7d704d9bcf64f6b066418342e85
hunterwb <hunter@hunterwb.com>
2019-02-23 20:21:26 -0800

    Show error dialog on exceptions

--- client/src/main/java/org/runestar/client/Main.kt
+++ client/src/main/java/org/runestar/client/Main.kt
@@ -2,6 +2,8 @@
 
 package org.runestar.client
 
+import com.alee.managers.style.StyleId
+import com.google.common.base.Throwables
 import io.reactivex.plugins.RxJavaPlugins
 import org.kxtra.slf4j.error
 import org.kxtra.slf4j.getLogger
@@ -11,9 +13,11 @@ import org.runestar.client.api.LafInstallation
 import org.runestar.client.common.JAV_CONFIG
 import org.runestar.client.common.MANIFEST_NAME
 import org.runestar.client.common.lookupClassLoader
-import java.io.IOException
 import java.util.jar.JarInputStream
 import java.util.jar.Manifest
+import javax.swing.JOptionPane
+import javax.swing.JScrollPane
+import javax.swing.JTextArea
 import javax.swing.SwingUtilities
 
 fun main() {
@@ -29,7 +33,8 @@ private fun checkUpToDate() {
     val serverManifest = JarInputStream(JAV_CONFIG.gamepackUrl.openStream()).use { it.manifest }
     val bundledManifest = lookupClassLoader.getResourceAsStream(MANIFEST_NAME).use { Manifest(it) }
     if (serverManifest != bundledManifest) {
-        throw IOException("Client is out of date")
+        showErrorDialog("Client is out of date")
+        System.exit(1)
     }
 }
 
@@ -38,7 +43,26 @@ private fun setupLogging() {
     RxJavaPlugins.setErrorHandler { e ->
         logger.warn(e) { "RxJavaPlugins error handler" }
     }
-    Thread.setDefaultUncaughtExceptionHandler { t, e ->
-        logger.error(e) { "Uncaught exception for thread ${t.name}" }
+    Thread.setDefaultUncaughtExceptionHandler { thread, exception ->
+        logger.error(exception) { "Uncaught exception" }
+        showErrorDialog(Throwables.getStackTraceAsString(exception))
+    }
+}
+
+private fun showErrorDialog(s: String) {
+    val runnable = Runnable {
+        val component = JScrollPane().apply {
+            setViewportView(JTextArea(s, 5, 40).apply {
+                isEditable = false
+            })
+            putClientProperty(StyleId.STYLE_PROPERTY, StyleId.scrollpaneUndecoratedButtonless)
+        }
+        JOptionPane.showMessageDialog(null, component, "Error", JOptionPane.ERROR_MESSAGE)
+    }
+
+    if (SwingUtilities.isEventDispatchThread()) {
+        runnable.run()
+    } else {
+        SwingUtilities.invokeAndWait(runnable)
     }
 }
