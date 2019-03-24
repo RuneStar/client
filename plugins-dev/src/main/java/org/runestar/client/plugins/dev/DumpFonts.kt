24ff159059bdfdf8c5a953301027e8936d32efc5
hunterwb <hunter@hunterwb.com>
2019-03-24 16:46:50 -0700

    Add DumpFonts dev plugin

---
+++ plugins-dev/src/main/java/org/runestar/client/plugins/dev/DumpFonts.kt
@@ -0,0 +1,76 @@
+package org.runestar.client.plugins.dev
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.runestar.client.game.raw.CLIENT
+import org.runestar.client.game.raw.access.XFont
+import org.runestar.client.plugins.spi.AbstractPlugin
+import org.runestar.client.plugins.spi.PluginSettings
+
+class DumpFonts : AbstractPlugin<PluginSettings>() {
+
+    override val defaultSettings = PluginSettings()
+
+    override fun start() {
+        val mapper = jacksonObjectMapper().enable(SerializationFeature.INDENT_OUTPUT)
+        val widget = CLIENT._Widget_()
+        val fontIds = CLIENT.indexCache13.archiveIds ?: throw Exception()
+        for (fontId in fontIds) {
+            widget.fontId = fontId
+            val xfont = widget.font ?: error(fontId)
+            val font = font(xfont)
+            val file = ctx.directory.resolve("$fontId.json")
+            mapper.writeValue(file.toFile(), font)
+        }
+    }
+
+    private fun font(f: XFont): Font {
+        val glyphs = ArrayList<Glyph>(256)
+        for (i in 0..255) {
+            if (f.advances[i] != 0) {
+                glyphs.add(Glyph(cp1252ToCodePoint(i), f.topBearings[i], f.heights[i], f.widths[i], f.advances[i], f.leftBearings[i], f.pixels[i].asList()))
+            }
+        }
+        val kerns = ArrayList<Kern>()
+        if (f.kerning != null) {
+            for (a in 0..255) {
+                for (b in 0..255) {
+                    val i = (a shl 8) + b
+                    val v = f.kerning[i].toInt()
+                    if (v != 0) {
+                        kerns.add(Kern(cp1252ToCodePoint(a), cp1252ToCodePoint(b), v))
+                    }
+                }
+            }
+        }
+        return Font(f.ascent, f.maxAscent, f.maxDescent, glyphs, kerns)
+    }
+
+    private fun cp1252ToCodePoint(n: Int): Int {
+        return String(byteArrayOf(n.toByte()), charset("windows-1252")).codePointAt(0)
+    }
+
+    data class Font(
+            val ascent: Int,
+            val maxAscent: Int,
+            val maxDescent: Int,
+            val glyphs: List<Glyph>,
+            val kernings: List<Kern>
+    )
+
+    data class Kern(
+            val a: Int,
+            val b: Int,
+            val size: Int
+    )
+
+    data class Glyph(
+            val codePoint: Int,
+            val topBearing: Int,
+            val height: Int,
+            val width: Int,
+            val advance: Int,
+            val leftBearing: Int,
+            val pixels: List<Byte>
+    )
+}
