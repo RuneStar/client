b04004344b68752c5955fed2ed9ae134ac201a4b
hunterwb <hunter@hunterwb.com>
2019-03-25 20:29:01 -0700

    Simplify Equipment

---
+++ game-api/src/main/java/org/runestar/client/game/api/EquipmentSlot.java
@@ -0,0 +1,22 @@
+package org.runestar.client.game.api;
+
+public final class EquipmentSlot {
+    
+    private EquipmentSlot() {}
+    
+    public static final int
+            HEAD=0,
+            CAPE=1,
+            NECK=2,
+            WEAPON=3,
+            BODY=4,
+            SHIELD=5,
+            ARMS=6,
+            LEGS=7,
+            HAIR=8,
+            HANDS=9,
+            FEET=10,
+            JAW=11,
+            RING=12,
+            AMMO=13;
+}
--- game-api/src/main/java/org/runestar/client/game/api/PlayerAppearance.kt
+++ game-api/src/main/java/org/runestar/client/game/api/PlayerAppearance.kt
@@ -1,34 +1,17 @@
 package org.runestar.client.game.api
 
 import org.runestar.client.game.raw.access.XPlayerAppearance
-import java.util.EnumMap
 
 inline class PlayerAppearance(val accessor: XPlayerAppearance) {
 
-    val items: Map<EquipmentSlot, Int> get() {
-        return EnumMap<EquipmentSlot, Int>(EquipmentSlot::class.java).apply {
-            val equip = accessor.equipment
-            for (v in EquipmentSlot.VALUES) {
-                val id = equip[v.id]
-                if (id >= 512) {
-                    check(v.supportsItems && v.isVisible)
-                    put(v, id - 512)
-                }
-            }
-        }
+    fun item(equipmentSlot: Int) : Int {
+        val v = accessor.equipment[equipmentSlot]
+        return if (v >= 512) v - 512 else -1
     }
 
-    val kit: Map<EquipmentSlot, Int> get() {
-        return EnumMap<EquipmentSlot, Int>(EquipmentSlot::class.java).apply {
-            val equip = accessor.equipment
-            for (v in EquipmentSlot.VALUES) {
-                val id = equip[v.id]
-                if (id in 256..511) {
-                    check(v.isVisible)
-                    put(v, id - 256)
-                }
-            }
-        }
+    fun kit(equipmentSlot: Int) : Int {
+        val v = accessor.equipment[equipmentSlot]
+        return if (v in 256..511) v - 256 else -1
     }
 
     val bodyColors: IntArray get() = accessor.bodyColors
