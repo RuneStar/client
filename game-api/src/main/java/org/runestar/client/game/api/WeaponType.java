0b8dbee0909cb8aa469eefc63560e2dcf3f02349
hunterwb <hunter@hunterwb.com>
2019-04-01 19:01:05 -0700

    Reformat WeaponType

--- game-api/src/main/java/org/runestar/client/game/api/WeaponType.java
+++ game-api/src/main/java/org/runestar/client/game/api/WeaponType.java
@@ -4,32 +4,33 @@ public final class WeaponType {
 
     private WeaponType() {}
 
-    public static final int UNARMED = 0;
-    public static final int AXE = 1;
-    public static final int _2 = 2;
-    public static final int _3 = 3;
-    public static final int _4 = 4;
-    public static final int _5 = 5;
-    public static final int SALAMANDER = 6;
-    public static final int CHINCHOMPA = 7;
-    public static final int _8 = 8;
-    public static final int _9 = 9;
-    public static final int _10 = 10;
-    public static final int PICKAXE = 11;
-    public static final int _12 = 12;
-    public static final int _13 = 13;
-    public static final int _14 = 14;
-    public static final int _15 = 15;
-    public static final int _16 = 16;
-    public static final int _17 = 17;
-    public static final int STAFF = 18;
-    public static final int _19 = 19;
-    public static final int WHIP = 20;
-    public static final int _21 = 21;
-    public static final int _22 = 22;
-    public static final int TRIDENT = 23;
-    public static final int _24 = 24;
-    public static final int _25 = 25;
-    public static final int _26 = 26;
-    public static final int BULWARK = 27;
+    public static final int
+            UNARMED=0,
+            AXE=1,
+            _2=2,
+            _3=3,
+            _4=4,
+            _5=5,
+            SALAMANDER=6,
+            CHINCHOMPA=7,
+            _8=8,
+            _9=9,
+            _10=10,
+            PICKAXE=11,
+            _12=12,
+            _13=13,
+            _14=14,
+            _15=15,
+            _16=16,
+            _17=17,
+            STAFF=18,
+            _19=19,
+            WHIP=20,
+            _21=21,
+            _22=22,
+            TRIDENT=23,
+            _24=24,
+            _25=25,
+            _26=26,
+            BULWARK=27;
 }
