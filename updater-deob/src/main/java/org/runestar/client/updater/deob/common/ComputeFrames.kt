db3e0d35eb136013538cfd705eafa1150e50affc
hunterwb <hunter@hunterwb.com>
2019-06-03 20:51:47 -0700

    Cleanup ComputeFrames

--- updater-deob/src/main/java/org/runestar/client/updater/deob/common/ComputeFrames.kt
+++ updater-deob/src/main/java/org/runestar/client/updater/deob/common/ComputeFrames.kt
@@ -16,36 +16,34 @@ object ComputeFrames : Transformer {
         writeJar(classNodes, destination) { Writer(classNames) }
     }
 
-    private class Writer(private val classNames: Map<String, ClassNode>) : ClassWriter(ClassWriter.COMPUTE_FRAMES) {
+    private class Writer(private val classNames: Map<String, ClassNode>) : ClassWriter(COMPUTE_FRAMES) {
 
         companion object {
             val OBJECT_INTERNAL_NAME: String = Type.getInternalName(Any::class.java)
         }
 
         override fun getCommonSuperClass(type1: String, type2: String): String {
-            if (isAssignable(type1, type2, classNames)) return type1
-            if (isAssignable(type2, type1, classNames)) return type2
+            if (isAssignable(type1, type2)) return type1
+            if (isAssignable(type2, type1)) return type2
             var t1 = type1
             do {
                 t1 = checkNotNull(superClassName(t1, classNames))
-            } while (!isAssignable(t1, type2, classNames))
+            } while (!isAssignable(t1, type2))
             return t1
         }
 
-        private fun isAssignable(to: String, from: String, classNames: Map<String, ClassNode>): Boolean {
+        private fun isAssignable(to: String, from: String): Boolean {
             if (to == from) return true
             val sup = superClassName(from, classNames) ?: return false
-            if (isAssignable(to, sup, classNames)) return true
-            return interfaceNames(from, classNames).any { isAssignable(to, it, classNames) }
+            if (isAssignable(to, sup)) return true
+            return interfaceNames(from).any { isAssignable(to, it) }
         }
 
-        private fun interfaceNames(type: String, classNames: Map<String, ClassNode>): List<String> {
+        private fun interfaceNames(type: String): List<String> {
             return if (type in classNames) {
-                val c = classNames.getValue(type)
-                c.interfaces
+                classNames.getValue(type).interfaces
             } else {
-                val c = Class.forName(type.replace('/', '.'))
-                c.interfaces.map { Type.getInternalName(it) }
+                Class.forName(type.replace('/', '.')).interfaces.map { Type.getInternalName(it) }
             }
         }
 
