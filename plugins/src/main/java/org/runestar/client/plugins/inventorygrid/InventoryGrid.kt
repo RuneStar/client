3e8846b00b649d716d7d4556b69beaf8a20355d2
hunterwb <hunter@hunterwb.com>
2019-06-15 20:25:00 -0700

    Fix InventoryGrid delay

--- plugins/src/main/java/org/runestar/client/plugins/inventorygrid/InventoryGrid.kt
+++ plugins/src/main/java/org/runestar/client/plugins/inventorygrid/InventoryGrid.kt
@@ -4,6 +4,7 @@ import org.runestar.client.api.forms.RgbaForm
 import org.runestar.client.api.util.DisposablePlugin
 import org.runestar.client.game.api.live.LiveCanvas
 import org.runestar.client.game.api.live.Components
+import org.runestar.client.game.raw.CLIENT
 import org.runestar.client.plugins.spi.PluginSettings
 
 class InventoryGrid : DisposablePlugin<InventoryGrid.Settings>() {
@@ -16,6 +17,7 @@ class InventoryGrid : DisposablePlugin<InventoryGrid.Settings>() {
         val color = settings.color.value
         add(LiveCanvas.repaints.subscribe { g ->
             val inv = Components.dragInventory ?: return@subscribe
+            if (CLIENT.itemDragDuration <= 5) return@subscribe
             if (!inv.isActive) return@subscribe
             g.color = color
             for ((item, shape) in inv.items) {
