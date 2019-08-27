f80b524282f3407fdb9c389ded837aaf06707313
hunterwb <hunter@hunterwb.com>
2019-08-26 22:04:40 -0700

    Start web module

---
+++ web/src/main/java/org/runestar/client/web/hiscore/HiscoreClient.kt
@@ -0,0 +1,18 @@
+package org.runestar.client.web.hiscore
+
+import org.runestar.client.web.bodyHandler
+import java.net.http.HttpClient
+import java.net.http.HttpResponse
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.CompletableFuture
+
+internal class HiscoreClient(private val httpClient: HttpClient) {
+
+    operator fun get(request: HiscoreRequest): CompletableFuture<HiscoreResult?> {
+        return httpClient.sendAsync(request.toHttpRequest(), bodyHandler).thenApply { it.body()?.let { HiscoreResult.of(it) } }
+    }
+
+    private companion object {
+        val bodyHandler = bodyHandler(HttpResponse.BodySubscribers.ofLines(StandardCharsets.US_ASCII))
+    }
+}
