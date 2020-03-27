8d1f17fa0f4ae113fa3a78522712b3348e7b9cf7
hunterwb <hunter@hunterwb.com>
2020-03-27 09:38:08 -0700

    Start 189

--- updater-mapper-standard/src/main/java/org/runestar/client/updater/mapper/std/classes/SecureRandomCallable.kt
+++ updater-mapper-standard/src/main/java/org/runestar/client/updater/mapper/std/classes/SecureRandomCallable.kt
@@ -4,9 +4,11 @@ import org.runestar.client.updater.mapper.IdentityMapper
 import org.runestar.client.updater.mapper.predicateOf
 import org.runestar.client.updater.mapper.type
 import org.runestar.client.updater.mapper.Class2
+import org.runestar.client.updater.mapper.and
 import java.util.concurrent.Callable
 
 class SecureRandomCallable : IdentityMapper.Class() {
 
     override val predicate = predicateOf<Class2> { it.interfaces == listOf(Callable::class.type) }
+            .and { it.instanceFields.isEmpty() }
 }
